20f639332a4cf7dce4fb4a051de43e33fb277d21
Seva-Sh <skimin1992@hotmail.com>
2024-01-16 15:34:37 +0400

    Wrote in text, description of the program

---
+++ mixed_messages.js
@@ -0,0 +1,4 @@
+// Create random message generator. 
+// These will be inspirational quotes from 3 randomly selected poets 
+//     Each poet will have 3 random quotes stored and selected, 
+//     once the author is randomly selected.
