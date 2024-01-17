4f1798e4bf2550562f06a2d9e5baed5a2aaf47ae
Seva-Sh <skimin1992@hotmail.com>
2024-01-17 08:00:36 +0400

    Added message components

--- mixed_messages.js
+++ mixed_messages.js
@@ -2,3 +2,18 @@
 // These will be inspirational quotes from 3 randomly selected poets 
 //     Each poet will have 3 random quotes stored and selected, 
 //     once the author is randomly selected.
+
+const quotes = {
+    "Winston Churchill": ["Success is not final, failure is not fatal, it is the courage to continue that counts.", 
+                          "To improve is to change, so to be perfect is to change often.",
+                          "Every man should ask himself each day whether he is not too readily accepting negative solutions."],
+    "Thomas Edison":     ["I have not failed. I've just found 10,000 ways that won't work.",
+                          "There's a better way to do - Find it.",
+                          "A genius is often merely a talented person who has done all of his or her homework."],
+    "Steve Jobs":        ["Innovation distinguishes between a leader and a follower.",
+                          "Your time is limited, so don’t waste it living someone else’s life.",
+                          "Stay hungry. Stay foolish."]
+
+}
+
+console.log(quotes)
